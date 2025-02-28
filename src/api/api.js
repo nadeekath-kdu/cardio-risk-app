d6a1959356a7c9e5f4052669af13e1e92d489fb8
Nadeekakdu <151503941+nadeekath-kdu@users.noreply.github.com>
2025-02-28 15:40:08 +0530

    fixed prediction issue

--- src/api/api.js
+++ src/api/api.js
@@ -4,8 +4,11 @@ const API_URL = "https://cardio-risk-prediction.onrender.com/predict";
 
 export const getPrediction = async (formData) => {
     try {
+
         const response = await axios.post(API_URL, formData);
-        return response.data.risk_prediction;
+        console.log("Risk Level1:", response.data.risk_level);
+
+        return response.data.risk_level;
     } catch (error) {
         console.error("Error:", error);
         return "Error making prediction";
--- src/components/Prediction.js
+++ src/components/Prediction.js
@@ -1,14 +1,27 @@
 import React from "react";
 
 const Prediction = ({ prediction, setPrediction }) => {
+    console.log("Predictions:", prediction);
     if (prediction === null) return null;
 
-    const isRisk = prediction !== 0;
+    // Define risk levels and corresponding messages
+    const riskLevels = ["<10%", "10% to <20%", "20% to <30%", "30% to <40%", ">=40%"];
+    const riskMessages = [
+        "✅ Very Low Risk",
+        "⚠️ Low Risk",
+        "⚠️ Moderate Risk",
+        "⚠️ High Risk",
+        "⚠️ Very High Risk"
+    ];
+    console.log("prediction:", prediction);
+    // Determine the risk message based on the prediction
+    const riskMessage = riskMessages[prediction];
+    const riskLevel = riskLevels[prediction];
 
     return (
-        <div className={`text-center mt-4 alert ${isRisk ? "alert-danger" : "alert-success"}`}>
-            <h3>{isRisk ? "⚠️ High Risk Detected" : "✅ No Risk Detected"}</h3>
-            <p className="fw-bold">Prediction: {prediction}</p>
+        <div className={`text-center mt-4 alert ${prediction >= 1 ? "alert-danger" : "alert-success"}`}>
+            <h3>{riskMessage}</h3>
+            <p className="fw-bold">Risk Level: {prediction}</p>
             {/* <button className="btn btn-outline-secondary mt-2" onClick={() => setPrediction(null)}>Clear Result</button> */}
         </div>
     );
